15824945e5d18c648b864328f6bcc7802578285f
felipe91v <felipe-viana91@hotmail.com>
2019-12-12 11:10:32 -0300

    arrumando estilo

--- jogodavelha.js
+++ jogodavelha.js
@@ -22,58 +22,60 @@ function play(a){
 
  //div que mostra a vez do jogador
 let player = document.querySelector("div#player")
-player.innerHTML = "<h1><strong>agora é a vez do</strong></h1>"
+player.innerHTML = "<strong>agora é a vez do</strong>"
 let img = document.createElement("img")
 img.src="x.png"
 document.getElementById("player").appendChild(img)
 
 //função que cria as jogadas adicionando as imagens
-function jogada(){
-    play(vez)
+function jogada(){ 
+   play(vez)
     let casa = document.getElementById(this.id)
     let imagem = document.createElement("img")    
     if(vez%2==0){
-      casa.setAttribute("jogada","X")      
+      casa.setAttribute("jogada","x")      
        imagem.src="x.png"
       } else {
-        casa.setAttribute("jogada","O")
+        casa.setAttribute("jogada","o")
        imagem.src="bola.png" 
     } 
     document.getElementById(this.id).appendChild(imagem)
-    vez++
     vencedor()
+    vez++
       }
    
    //FUNÇÃO QUE VALIDA O VENCEDOR. *NOTE A FUNÇÃO É CHAMADA DENTRO DA FUNÇÃO 
    //JOGADA PARA VALIDAR A CADA JOGADA SE HOUVE VENCEDOR OU NÃO.
-   function vencedor(){
-    let casa0 = document.querySelector("div#casa0").getAttribute("jogada")
-    let casa1 = document.querySelector("div#casa1").getAttribute("jogada")
-    let casa2 = document.querySelector("div#casa2").getAttribute("jogada")
+   async function vencedor(){
+    let a1 = document.querySelector("div#casa0").getAttribute("jogada")
+    let a2 = document.querySelector("div#casa1").getAttribute("jogada")
+    let a3 = document.querySelector("div#casa2").getAttribute("jogada")
     
-    let casa3 = document.querySelector("div#casa3").getAttribute("jogada")
-    let casa4 = document.querySelector("div#casa4").getAttribute("jogada")
-    let casa5 = document.querySelector("div#casa5").getAttribute("jogada")
+    let b1 = document.querySelector("div#casa3").getAttribute("jogada")
+    let b2 = document.querySelector("div#casa4").getAttribute("jogada")
+    let b3 = document.querySelector("div#casa5").getAttribute("jogada")
     
-    let casa6 = document.querySelector("div#casa6").getAttribute("jogada")
-    let casa7 = document.querySelector("div#casa7").getAttribute("jogada")
-    let casa8 = document.querySelector("div#casa8").getAttribute("jogada")
+    let c1 = document.querySelector("div#casa6").getAttribute("jogada")
+    let c2 = document.querySelector("div#casa7").getAttribute("jogada")
+    let c3 = document.querySelector("div#casa8").getAttribute("jogada")
     
-    let ganhador
+    let ganhador = ""
 
     if
-        ((casa0 == casa1 && casa0 == casa2)||(casa0 == casa3 && casa0 == casa6)||
-        (casa0 == casa4 && casa0 == casa8) && (casa0 != "")){
-         ganhador = casa0
-       } else if ((casa4 == casa3 && casa4 == casa5)||
-            (casa4 == casa1 && casa4 == casa7) || (casa4 == casa2 && casa4 == casa6) && (casa4 != "")){
-            ganhador = casa4
-       } else if ((casa8 == casa6 && casa8 == casa7)||
-            (casa8 == casa5 && casa8 == casa2) && (casa8 != "")){
-             ganhador = casa8
+        (((a1 == a2 && a1 == a3)||(a1 == b1 && a1 == c1)||(a1 == b2 && a1 == c3)) && a1 != ""){
+         ganhador = a1
+       } else if (((b2 == a2 && b2 == c2)||(b2 == b1 && b2 == b3) || (b2 == a3 && b2 == c1)) && b2 != ""){
+            ganhador = b2
+       } else if (((c3 == c2 && c3 == c1)||(c3 == b3 && c3 == a3)) && c3 != ""){
+             ganhador = c3
        } 
      
        if(ganhador != ""){
-          alert(`o ganhador foi ${ganhador.toUpperCase()}`)
+          await sleep(50)
+          alert(`o ganhador foi ${ganhador}`)
          }
+      }
+
+      function sleep(ms){
+        return new Promise(resolve => setTimeout(resolve,ms))
       }
